1ca628ec6944b5c2ea85b6ad7779d6fded5ffa68
乔志 <1154025883@qq.com>
2020-02-11 18:29:06 +0800

    增加类

---
+++ src/com/atguigu/bean/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author Administrator-qz
+ * @create 2020-02-11 18:27
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("lll");
+    }
+}
